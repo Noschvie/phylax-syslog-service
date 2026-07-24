f679dfc7cdce7f07e738fa9d328a6ab3487a2fa9
noschvie <48217146+Noschvie@users.noreply.github.com>
2026-07-24 14:25:13 +0200

    chore: add ESLint configuration for JavaScript files

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import airbnb from 'eslint-config-airbnb-base';
+import importPlugin from 'eslint-plugin-import';
+
+export default [
+  {
+    ignores: ['node_modules/', 'coverage/', 'dist/', 'logs/'],
+  },
+  {
+    files: ['src/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': 'warn',
+      'import/extensions': 'off',
+      'no-underscore-dangle': 'off',
+      'class-methods-use-this': 'off',
+    },
+  },
+];
