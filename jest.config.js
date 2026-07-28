0011dfb964be81960a60be633d41f40cf58bb921
noschvie <48217146+Noschvie@users.noreply.github.com>
2026-07-28 03:04:18 +0200

    Configure SYSLOG_PORT for tests and improve error handling in syslogService

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ export default {
   coveragePathIgnorePatterns: ['/node_modules/'],
   testMatch: ['**/test/**/*.test.js'],
   collectCoverageFrom: ['src/**/*.js'],
-  testTimeout: 30000, // 30s reicht – 180s war viel zu lang
-  forceExit: true,    // Beendet Jest auch bei offenen Handles
-  detectOpenHandles: true, // Zeigt an, welche Handles noch offen sind
+  testTimeout: 30000,
+  forceExit: true,
+  detectOpenHandles: true,
+  globalSetup: './test/jest.globalSetup.js',
 };
